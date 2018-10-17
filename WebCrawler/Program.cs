00157544f8d39e93e4477fdbfa906d937070f2f6
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-10-17 09:02:59 +0500

    Add project files.

---
+++ WebCrawler/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+
+namespace WebCrawler
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new WebClient();
+            var text = client.DownloadString("https://www.facebook.com/");
+            Console.WriteLine(text);
+            Console.ReadKey();
+        }
+    }
+}
